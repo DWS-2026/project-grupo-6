a7044905d7aa8f4da08e473a5b13e0a980d10aee
eLNiTa <e.arcaz.2022@alumnos.urjc.es>
2026-02-13 11:22:57 +0100

    Adding User Entity

--- project-grupo-6/src/main/java/com/example/projectgrupo6/repositories/ProductRepository.java
+++ project-grupo-6/src/main/java/com/example/projectgrupo6/repositories/ProductRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.projectgrupo6.domain.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    // Filtros basados en tu shop.html
+    // Filters based of shop.html
     List<Product> findByCategory(String category);
     List<Product> findByPowerSource(String powerSource);
     List<Product> findByBrand(String brand);
