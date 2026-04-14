ce9ed3ffbb1bbe98759aa4ef153ab65aaec7b04c
eLNiTa <e.arcaz.2022@alumnos.urjc.es>
2026-04-14 09:15:29 +0200

    Adding structure to the project on DTOs side

---
+++ project-grupo-6/src/main/java/com/example/projectgrupo6/dto/basicDtos/UserBasicDTO.java
@@ -0,0 +1,9 @@
+package com.example.projectgrupo6.dto.basicDtos;
+
+public record UserBasicDTO (
+        Long id,
+        String firstname,
+        String lastname,
+        String email
+        //image (?)
+) {}
--- project-grupo-6/src/main/java/com/example/projectgrupo6/dto/mappers/ImageMapper.java
+++ project-grupo-6/src/main/java/com/example/projectgrupo6/dto/mappers/ImageMapper.java
@@ -1,6 +1,7 @@
-package com.example.projectgrupo6.dto;
+package com.example.projectgrupo6.dto.mappers;
 
 import com.example.projectgrupo6.domain.Image;
+import com.example.projectgrupo6.dto.ImageDTO;
 import org.mapstruct.Mapper;
 
 @Mapper (componentModel = "spring")
